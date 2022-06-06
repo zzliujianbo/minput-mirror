22def5aad2811dda11df4251ab30d6e424e4e56f
zzliujianbo <zzliujianbo@foxmail.com>
2022-06-06 11:20:23 +0800

    完善初始化

---
+++ src/dev/server.rs
@@ -0,0 +1,5 @@
+use log::info;
+
+pub fn start() {
+    info!("服务端模式");
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,16 @@
 use env_logger::{fmt::Color, Builder, Env};
 use lazy_static::lazy_static;
 use log::info;
+use rdev::display_size;
 use serde::{Deserialize, Serialize};
 use std::io::Write;
+mod dev;
+mod net;
 
 lazy_static! {
     ///配置对象
-    static ref CONFIG: Config = init_config();
+    static ref CONFIG: Config = get_config();
+    static ref DISPLAY: Display = get_display();
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -17,10 +21,34 @@ pub struct Config {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct ConfigServer {}
+pub struct ConfigServer {
+    ///服务器监听地址
+    pub ip: String,
+    ///服务器监听端口
+    pub port: u16,
+}
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct ConfigClient {}
+pub struct ConfigClient {
+    ///服务器地址
+    pub server_ip: String,
+    ///服务器端口
+    pub server_port: u16,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Display {
+    ///屏幕分辨率宽度
+    pub width: u64,
+    ///屏幕分辨率高度
+    pub height: u64,
+}
+
+impl Display {
+    pub fn new(width: u64, height: u64) -> Self {
+        Display { width, height }
+    }
+}
 
 /// 初始化日志
 fn init_logger() {
@@ -50,15 +78,44 @@ fn init_logger() {
         .init();
 }
 
-///初始化配置
-fn init_config() -> Config {
+///获取配置
+fn get_config() -> Config {
     let f = std::fs::File::open("config.yaml").expect("读取配置文件失败");
     let config: Config = serde_yaml::from_reader(f).expect("配置文件解析失败");
     config
 }
 
+///获取屏幕分辨率
+fn get_display_size() -> (u64, u64) {
+    display_size().expect("获取屏幕分辨率失败")
+}
+
+///获取屏幕详情
+fn get_display() -> Display {
+    let (w, h) = get_display_size();
+    Display::new(w, h)
+}
+
+///启动服务
+fn start_service() {
+    match CONFIG.mode.as_str() {
+        "server" => {
+            dev::server::start();
+        }
+        "client" => {
+            dev::client::start();
+        }
+        _ => {
+            panic!("配置文件mode错误(允许值：server/client)");
+        }
+    }
+
+    info!("start {} success", CONFIG.mode);
+}
+
 pub fn start() {
     init_logger();
     info!("read config: {:#?}", *CONFIG);
-    info!("start {}", CONFIG.mode);
+    info!("display: {:#?}", *DISPLAY);
+    start_service();
 }
