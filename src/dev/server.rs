eece67cf513f0314a593eec39aeeeef32bf07ee7
zzliujianbo <zzliujianbo@foxmail.com>
2022-06-16 18:04:35 +0800

    tx rx

--- src/dev/server.rs
+++ src/dev/server.rs
@@ -1,9 +1,5 @@
-use std::net::UdpSocket;
-use std::thread;
-
 use crate::{net::server, CONFIG};
 use anyhow::Result;
-use log::debug;
 use log::info;
 use log::warn;
 use rdev::listen;
@@ -12,18 +8,12 @@ use rdev::Event;
 pub fn start() -> Result<()> {
     //TODO 需要检测鼠标键盘是否存在，如果不存在则进行警告
     let server_config = CONFIG.server.as_ref().expect("配置文件错误");
-    //let (tx, rx) = std::sync::mpsc::channel::<Event>();
+    let (tx, rx) = std::sync::mpsc::channel::<Event>();
 
-    let udp = server::start(server_config.ip.as_str(), server_config.port)?;
+    server::start(server_config.ip.as_str(), server_config.port, rx)?;
 
     let handle_event = move |event: Event| {
-        //match event.event_type {
-        //    rdev::EventType::MouseMove { x, y } => (),
-        //    _ => println!("My callback {:?}", event),
-        //}
-        //tx.send(event)
-        //    .unwrap_or_else(|e| warn!("send event error: {:?}", e));
-        udp.send(event.into())
+        tx.send(event)
             .unwrap_or_else(|e| warn!("send event error: {:?}", e));
     };
     info!("start server success");
--- src/net/server.rs
+++ src/net/server.rs
@@ -1,11 +1,21 @@
 use anyhow::Result;
 use log::{debug, info, warn};
-use std::{net::UdpSocket, sync::Arc, thread};
+use rdev::{Event, EventType};
+use std::{
+    net::{SocketAddr, UdpSocket},
+    sync::{mpsc::Receiver, Arc},
+    thread,
+};
+
+use crate::net::protocol::Flag;
 
 use super::protocol::{Protocol, PROTOCOL_LEN};
 
 pub struct UdpServer {
+    //socket: Arc<UdpSocket>,
     socket: Arc<UdpSocket>,
+    active_client: Option<SocketAddr>,
+    clients: Vec<SocketAddr>,
 }
 
 impl UdpServer {
@@ -14,6 +24,8 @@ impl UdpServer {
         debug!("UdpSocket bind to {}:{}", ip, port);
         Ok(Self {
             socket: Arc::new(socket),
+            clients: vec![],
+            active_client: None,
         })
     }
 
@@ -26,17 +38,42 @@ impl UdpServer {
     }
 }
 
-pub fn start(ip: &str, port: u16) -> Result<UdpServer> {
-    let udp = UdpServer::new(ip, port)?;
-    let socket = udp.socket.clone();
+pub fn start(ip: &str, port: u16, rx: Receiver<Event>) -> Result<()> {
+    let udp = Arc::new(UdpServer::new(ip, port)?);
+    let udp_clone = udp.clone();
+
+    thread::spawn(move || loop {
+        for event in rx.iter() {
+            match event.event_type {
+                EventType::KeyPress(k) => {
+                    debug!("key down: {:?}", k);
+                    //udp_clone.send(protocol)?;
+                }
+                _ => {}
+            }
+        }
+    });
     thread::spawn(move || loop {
         // max 1472 bytes, mtu(1500) - udp header(8) - ip header(20) = 1472
         //每次传输报文控制在最大1472字节，防止分片传输
         //每次接收512字节，最长不超过512
         let mut buf = [0u8; PROTOCOL_LEN];
-        let recv = socket.recv_from(&mut buf);
+        //let recv = udp_clone.socket.recv_from(&mut buf);
+        let recv = udp_clone.socket.recv_from(&mut buf);
         if let Ok((len, addr)) = recv {
             let protocol = Protocol::from(&buf[..]);
+            match protocol.flag {
+                Flag::ClientInitConnection => {
+                    info!("client connect: {}", addr);
+                    //udp_clone.clients.push(addr);
+                }
+                _ => {
+                    warn!("unknown protocol: {:?}", protocol);
+                }
+            }
+            //if protocol.flag == Flag::ClientInitConnection {
+            //    udp_clone.clients.push(addr);
+            //}
             debug!(
                 "recv from {:?}, {:?}, {:?}",
                 addr.ip(),
@@ -47,5 +84,5 @@ pub fn start(ip: &str, port: u16) -> Result<UdpServer> {
             warn!("接收数据错误: {:?}", recv);
         }
     });
-    Ok(udp)
+    Ok(())
 }
